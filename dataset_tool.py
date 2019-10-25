dataset_tool fix for directory structure + fid calculation bug fix + small changes in train.py

dataset_tool.py
@@ -502,9 +502,21 @@ def create_celeba(tfrecord_dir, celeba_dir, cx=89, cy=121):
 
 #----------------------------------------------------------------------------
 
+def _get_all_files(path):
+    if os.path.isfile(path):
+        return [path]
+
+    possible_files = sorted(glob.glob(os.path.join(path, '*')))
+    return_list = []
+    for possible_file in possible_files:
+        return_list.extend(_get_all_files(possible_file))
+    return return_list
+
+
 def create_from_images(tfrecord_dir, image_dir, shuffle):
     print('Loading images from "%s"' % image_dir)
-    image_filenames = sorted(glob.glob(os.path.join(image_dir, '*')))
+    image_filenames = _get_all_files(image_dir)
+    print(f"detected {len(image_filenames)} images ...")
     if len(image_filenames) == 0:
         error('No input images found')
     img = np.asarray(PIL.Image.open(image_filenames[0]))
@@ -525,6 +537,8 @@ def create_from_images(tfrecord_dir, image_dir, shuffle):
                 img = img[np.newaxis, :, :] # HW => CHW
             else:
                 img = img.transpose([2, 0, 1]) # HWC => CHW
+            if img.shape[0] > 3:
+                img = img[:3, ...]
             tfr.add_image(img)
 
 #----------------------------------------------------------------------------

dnnlib/tflib/tfutil.py
@@ -222,7 +222,8 @@ def convert_images_from_uint8(*images, drange=[-1,1], nhwc_to_nchw=False):
     images = [tf.cast(image, tf.float32) for image in images]
     if nhwc_to_nchw:
         images = [tf.transpose(image, [0, 3, 1, 2]) for image in images]
-    return [(image - drange[0]) * ((drange[1] - drange[0]) / 255) for image in images]
+    images = [(image - drange[0]) * ((drange[1] - drange[0]) / 255) for image in images]
+    return images[0] if len(images) == 1 else images
 
 
 def convert_images_to_uint8(*images, drange=[-1,1], nchw_to_nhwc=False, shrink=1):
@@ -238,4 +239,5 @@ def convert_images_to_uint8(*images, drange=[-1,1], nchw_to_nhwc=False, shrink=1
         images = [tf.transpose(image, [0, 2, 3, 1]) for image in images]
     scale = 255 / (drange[1] - drange[0])
     images = [image * scale + (0.5 - drange[0] * scale) for image in images]
-    return [tf.saturate_cast(image, tf.uint8) for image in images]
+    images = [tf.saturate_cast(image, tf.uint8) for image in images]
+    return images[0] if len(images) == 1 else images